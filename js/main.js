d1cba66566fb5b85dd4d6f259078f2b03be89db1
jbGabantu <gabantuj@gmail.com>
2020-01-09 12:05:35 -0600

    Fix error with sw.js file

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var markers = []
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("../sw.js")
+      .register("./sw.js")
       .then(reg => console.log(`Service worker registration succeeded`, reg))
       .catch(err => console.log(`Service worker registration failed:, ${err}`));
   });
